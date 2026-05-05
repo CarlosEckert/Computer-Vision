d8fe3cd8aa4b5ef19147201b03fd458fcc5a76ce
carlos <carloseckert05@gmail.com>
2026-05-05 17:39:02 +0200

    first_yolo_commit

--- Opencv_Projects/Object_detection.py
+++ Opencv_Projects/Object_detection.py
@@ -4,12 +4,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 import mss
 import keyboard
+from ultralytics import YOLO
+
 
 
 # Braucht Rechte
+def draw_rectangle(frame, x, y, width, height):
+    cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 0, 0), thickness=2)
+
+
+def init_yolo(model_path='yolov8n.pt'):
+    return YOLO(model_path)
+
+
+def run_yolo_tracker(model, frame):
+    results = model.track(frame, persist=True)
+    if results[0].boxes is not None:
+        for box in results[0].boxes:
+            x1, y1, x2, y2 = (int(v) for v in box.xyxy[0])
+            draw_rectangle(frame, x1, y1, x2 - x1, y2 - y1)
+
+
 def use_camera():
     print("Starting usage of camera, Press ESC to quit.")
-    source = cv2.VideoCapture(0, cv2.CAP_MSMF)
+    model = init_yolo()
+
+    source = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
     win_name = 'Camera Preview'
     cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
@@ -18,18 +38,12 @@ def use_camera():
         has_frame, frame = source.read()
         if not has_frame:
             break
+
+        run_yolo_tracker(model, frame)
         cv2.imshow(win_name, frame)
 
     source.release()
     cv2.destroyWindow(win_name)
-
-
-def draw_rectangle(frame, x, y, width, height):
-    color = (0, 0, 0)
-    cv2.rectangle(frame, (x, y), (x + width, y + height), color, thickness=2)
-    cv2.imshow('Rectangle', frame)
-    cv2.waitKey(0)
-    cv2.destroyWindow('Rectangle')
 
 
 
